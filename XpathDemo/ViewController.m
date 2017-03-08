788122a3c5f7e2b0f5bb5f4b7ac46655874dd644
brant <brant@lhtcdeMac-mini.local>
2017-03-08 17:54:05 +0800

    插入数据库添加条件

--- XpathDemo/ViewController.m
+++ XpathDemo/ViewController.m
@@ -25,8 +25,8 @@
     
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        for (int i = 53; i < 54; i++) {
-            NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.gs5000.cn/gs/shenhua/%d.html", i]]];
+        for (int i = 5; i < 24573; i++) {
+            NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.gs5000.cn/gs/chengyu/%d.html", i]]];
             dispatch_async(self.queue, ^{
                 TFHpple *hpple = [TFHpple hppleWithHTMLData:data];
                 NSArray *eles = [hpple searchWithXPathQuery:@"//div[@class='place']/a[2] | //h2 | //div[@class='content']/table[1]/tr/td"];
@@ -35,14 +35,17 @@
                     category = ((TFHppleElement *) eles[0]).content;
                     title = ((TFHppleElement *) eles[1]).content;
                     content = ((TFHppleElement *) eles[2]).content;
-                    for (TFHppleElement *ele in eles) {
-                        NSLog(@"title: %@", ele.content);
+                    if (title.length > 0 && content.length > 0) {
+                        [[DBManager shareInstance] insert:category title:title content:content];
                     }
                     
-                    [[DBManager shareInstance] insert:category title:title content:content];
                 }
             });
+            
+            
         }
+        
+        NSLog(@"完成");
         
         
     });
